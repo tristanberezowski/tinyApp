c8054cbba9900911cc122b28d4c1a86a4889d07a
Tristan <tristanberezowski@gmail.com>
2019-02-12 20:28:55 +0000

    adding ejs files helloworld and urlsindex

--- expressServer.js
+++ expressServer.js
@@ -12,12 +12,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urlsIndex", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("helloWorld", templateVars);
 });
 
 app.listen(PORT, () => {
