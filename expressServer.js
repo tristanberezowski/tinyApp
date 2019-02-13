ec9456cdc7e819aac77cf630297947f11aae2be1
Tristan <tristanberezowski@gmail.com>
2019-02-13 00:26:48 +0000

    updated front end

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,6 +12,10 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urlsNew");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
